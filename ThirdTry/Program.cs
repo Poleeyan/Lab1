ee11bf1a282d2a964a58b5632f103f9825dd72e1
Poleeyan <v0va12yt@gmail.com>
2024-09-06 19:40:54 +0300

    9/6/2024

--- ThirdTry/Program.cs
+++ ThirdTry/Program.cs
@@ -14,17 +14,20 @@ namespace ThirdTry
             string name = "Столовник Володимир";
             int age = 18;
             Console.WriteLine("Привiт, мене звати {0}, менi {1} рокiв", name, age);
+            // end
 
             // second
-            // a*a -- Math.Pow(a,2)
-            //Console.Write(result)
             int a = 3;
             int b = 8;
             double result;
-            result = (Math.Pow(a+b,4))-(Math.Pow(a,4))+4*(Math.Pow(a,3))*b+6*(Math.Pow(a,2))*(Math.Pow(b,2))/(Math.Pow(b,2))*4*a+(Math.Pow(b,4));
-            Console.WriteLine("result = {0}", result);
-            //Console.WriteLine($"result = {result}");
+            result = ((Math.Pow(a + b, 4)) - (Math.Pow(a, 4)) + 4 * (Math.Pow(a, 3)) * b + 6 * (Math.Pow(a, 2)) * (Math.Pow(b, 2))) / ((Math.Pow(b, 2)) * 4 * a + (Math.Pow(b, 4)));
+            Console.WriteLine($"result = {result}");
+            //end
 
+            //third
+            // ебу рот этой хуйне блядской сука
+            Console.WriteLine($"result = {result * -1}");
+            //end
 
             Console.ReadKey(true);
         }
